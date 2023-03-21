c554cb5b1c3896b1a36809794849d766a8636d3c
Jatin Suthar <jatin.suthar.here@gmail.com>
2023-03-21 22:03:03 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Jatin Suthar on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
